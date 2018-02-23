5a2818cb3a5f271dae2c06f73aeba58d93fe7c37
Cedric Goh <chonghao@umich.edu>
2018-02-23 10:40:13 -0500

    implemented arg check

--- rpn.py
+++ rpn.py
@@ -6,6 +6,7 @@ operators = {
     '+': operator.add,
     '-': operator.sub,
 }
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
@@ -19,6 +20,9 @@ def calculate(arg):
             result = function(arg1, arg2)
             stack.append(result)
         print(stack)
+    if len(stack) != 1:
+        raise TypeError 
+        
     return stack.pop()
 
 def main():
