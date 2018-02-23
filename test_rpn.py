79073a2d07a483a1b3a0c2750ede4361092ea1fe
Cedric Goh <chonghao@umich.edu>
2018-02-23 10:20:27 -0500

    implemented subtraction test

--- test_rpn.py
+++ test_rpn.py
@@ -5,6 +5,9 @@ class TestBasics(unittest.TestCase):
     def testAdd(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
-    def test_adds(self):
+    def testAdds(self):
         result = rpn.calculate('1 1 + 2 +')
         self.assertEqual(4, result)
+    def testSub(self):
+        result = rpn.calculate('5 2 -')
+        self.assertEqual(3, result)
