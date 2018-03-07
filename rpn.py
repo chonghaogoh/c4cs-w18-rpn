134208ad8e0d63b593a9788109c51b7d1c7faa0d
Cedric Goh <chonghao@umich.edu>
2018-03-06 22:44:38 -0500

    added travis file

--- rpn.py
+++ rpn.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 import operator 
+import logging
+import sys
+import math
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
 
 operators = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
-    '/': operator.floordiv,
+    '/': operator.truediv,
+    '^': operator.pow,
+    '.': operator.floordiv,
+    'and': operator.and_, 
+    'or': operator.or_,
 }
 
 def calculate(arg):
@@ -16,11 +28,26 @@ def calculate(arg):
             value = int(token)
             stack.append(value)
         except ValueError:
-            function = operators[token]
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
-            stack.append(result)
+            if token == '%':
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                result = arg1 * arg2 / 100 + arg1
+                stack.append(result)
+            elif token == '!':
+                arg1 = stack.pop()
+                arg1 = math.factorial(arg1)
+                stack.append(arg1)
+            elif token == 'not':
+                arg1 = int(stack.pop())
+                result = ~arg1 
+                stack.append(result)
+            else :
+                function = operators[token]
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                result = function(arg1, arg2)
+                stack.append(result)
+        logger.debug(stack)
     if len(stack) != 1:
         raise TypeError 
 
--- test_rpn.py
+++ test_rpn.py
@@ -29,3 +29,24 @@ class TestBasics(unittest.TestCase):
     def testChain(self):
         result = rpn.calculate('2 2 + 1 - 3 *')
         self.assertEqual(9, result)
+    def testPow(self):
+        result = rpn.calculate('4 3 ^')
+        self.assertEqual(64, result)
+    def testPercent(self):
+        result = rpn.calculate('0 72 + 5 %')
+        self.assertEqual(75.6, result)
+    def testIntDiv(self):
+        result = rpn.calculate('5 2 .')
+        self.assertEqual(2, result)
+    def testFact(self):
+        result = rpn.calculate('4 !')
+        self.assertEqual(24, result)
+    def testAnd(self):
+        result = rpn.calculate('2 1 and')
+        self.assertEqual(2 & 1, result)
+    def testOr(self):
+        result = rpn.calculate('4 3 or')
+        self.assertEqual(4 | 3, result)
+    def testNor(self):
+        result = rpn.calculate('5 not')
+        self.assertEqual(~5, result)
